0ae4272f2da8ca68b9d048e13fea986f71f5b778
Federico Barresi <baf40684@hs-regensburg.de>
2020-01-24 17:12:30 +0100

    imported workspace

---
+++ Test1/src/fede/algo/Prime.java
@@ -0,0 +1,42 @@
+package fede.algo;
+
+
+public class Prime {
+
+	public static void main(String[] args) {
+		int n = 50;
+		int[] primes = GetPrimes(n);
+		String[] primesStrings = new String[primes.length];
+		for (int i = 0; i < primes.length; i++) {
+			primesStrings[i] = String.valueOf(primes[i]);
+		}
+		System.out.println("["+String.join(",", primesStrings)+"]");
+
+	}
+
+	
+	public static int[] GetPrimes(int n) 
+	{
+		int[] primes = new int[n];
+		
+		int k = 0;
+		for (int i = 1; i < n+1; i++) {
+			if(IsMultipleOfAny(i, primes) ) continue;
+			else {
+				primes[k] = i;
+				k++;
+			} 
+		}
+		return primes;
+		
+	}
+	
+	public static boolean IsMultipleOfAny(int n, int[] numbers) 
+	{
+		for (int i = 1; i < numbers.length; i++) {
+			if(numbers[i]== 0) break;
+			if(n%numbers[i] == 0) return true;
+		}
+		return false;
+	}
+}
